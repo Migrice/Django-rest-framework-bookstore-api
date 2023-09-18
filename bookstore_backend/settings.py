a270a979423651ddda1b03c63fb8f3aa690cc35b
migrice <efomenakuete@gmail.com>
2023-09-18 13:41:07 +0100

    Add deployed domain

--- bookstore_backend/settings.py
+++ bookstore_backend/settings.py
@@ -130,5 +130,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:3000', 'https://nextjs-bookstore-app-git-main-migrice.vercel.app'
 ]
